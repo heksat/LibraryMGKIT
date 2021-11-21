f0a3d91c530aeaa5729d4a9cc78f0e76aa554111
Rsokolov <rsokolov@croc.ru>
2021-11-21 21:16:51 +0300

    Controllers

---
+++ LibraryRestApi/Controllers/GenreController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LibraryRestApi.Controllers
+{
+    public class GenreController:Controller
+    {
+    }
+}
